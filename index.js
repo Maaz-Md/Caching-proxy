3ee8906b2b5cc8e60ab41f178d9291874a85e345
MaazMd <muazbellary111@gmail.com>
2026-07-14 08:25:47 +0530

    configured index.js file into a function

--- index.js
+++ index.js
@@ -1,81 +1,83 @@
 const express = require("express");
 
-const app = express();
+function startServer({ port, origin }) {
 
-const cache = new Map();
+    const app = express();
 
-const PORT = 3000;
+    const cache = new Map();
 
-const ORIGIN = "https://dummyjson.com";
+    app.get("/:resource", async (req, res) => {
 
-app.get("/:resource", async (req, res) => {
-
-    try {
-        
-        const { resource } = req.params;
-
-        if (cache.has(resource)) {
+        try {
             
-            console.log("Cache Hit");
+            const { resource } = req.params;
 
-            const cachedResponse = cache.get(resource);
-            
-            for (const [key, value] of Object.entries(cachedResponse.headers)) {
-                res.setHeader(key, value);
+            if (cache.has(resource)) {
+                
+                console.log("Cache Hit");
+
+                const cachedResponse = cache.get(resource);
+                
+                for (const [key, value] of Object.entries(cachedResponse.headers)) {
+                    res.setHeader(key, value);
+                }
+
+                // custom header 
+                res.setHeader("X-Cache", "HIT");
+
+                return res.json(cachedResponse.body);
             }
 
-            // custom header 
-            res.setHeader("X-Cache", "HIT");
+            console.log("Cache Miss");
 
-            return res.json(cachedResponse.body);
+
+            // fetch from origin server
+            const response = await fetch(`${origin}/${resource}`);
+
+
+            if (!response.ok) {
+
+                const error = await response.json();
+                
+                return res.status(response.status).json(error);
+            }
+
+            // converts JSON to js object
+            const body = await response.json();
+
+
+            const responseToCache = {
+                headers: Object.fromEntries(response.headers.entries()),
+                body
+            }
+
+            //Caches response
+            cache.set(resource, responseToCache);
+
+            //forward headers to client
+            response.headers.forEach((value, key) => {
+                res.setHeader(key, value);
+            })
+
+            // custom header
+            res.setHeader("X-Cache", "MISS");
+
+            res.json(body);
+
+
+        } catch (error) {
+            console.error(error);
+
+            res.status(500).json({
+                message: "Something went wrong."
+            });
         }
+    });
 
-        console.log("Cache Miss");
+    app.listen(port, () => {
+        console.log(`Proxy Server is running on  http://localhost:${port}`);
+    });
 
+}
 
-        // fetch from origin server
-        const response = await fetch(`${ORIGIN}/${resource}`);
-
-
-        if (!response.ok) {
-
-            const error = await response.json();
-            
-            return res.status(response.status).json(error);
-        }
-
-        // converts JSON to js object
-        const body = await response.json();
-
-
-        const responseToCache = {
-            headers: Object.fromEntries(response.headers.entries()),
-            body
-        }
-
-        //Caches response
-        cache.set(resource, responseToCache);
-
-        //forward headers to client
-        response.headers.forEach((value, key) => {
-            res.setHeader(key, value);
-        })
-
-        // custom header
-        res.setHeader("X-Cache", "MISS");
-
-        res.json(body);
-
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            message: "Something went wrong."
-        });
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Proxy Server is running on  http://localhost:${PORT}`);
-});
+module.exports = startServer;
